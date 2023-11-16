980811f31741c52f8c02ba521731af2689d79534
testhariikr <testhariikr@gmail.com>
2023-11-16 18:19:09 +0530

    h

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {Routes,Route} from "react-router-dom"
 import Register from "./register";
 import Login from "./login";
 import Forgetpassword from "./forgotpassword";
