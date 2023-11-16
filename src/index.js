6198e14beb3609a3f49db6817aabfb0c11f2328f
testhariikr <testhariikr@gmail.com>
2023-11-16 18:06:05 +0530

    first

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {  Router , BrowserRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <App/>
     </BrowserRouter>
   
 );
